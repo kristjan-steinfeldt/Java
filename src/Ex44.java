a502861a5f8216428e72e62b4b90c5bd09f81428
kristjan <kristjan.steinfeldt@voco.ee>
2022-09-08 02:49:42 -0700

    Excercise ex 44 done

---
+++ src/Ex44.java
@@ -0,0 +1,19 @@
+public class Ex44 {
+        public static int least(int number1, int number2) {
+
+            if (number1>number2){
+                return number2;
+            }
+
+            else {
+                return number1;
+            }
+        }
+
+        public static void main(String[] args) {
+            int answer =  least(2, 7);
+            System.out.println("Least: " + answer);
+        }
+
+    }
+
