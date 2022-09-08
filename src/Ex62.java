2ff4454e48ec51d4cce5cf0f3c340b9d14a71534
kristjan <kristjan.steinfeldt@voco.ee>
2022-09-08 04:36:54 -0700

    Exercise ex 62 done

---
+++ src/Ex62.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Ex62 {
+    public static void removeLast(ArrayList<String> list){
+        list.remove(3);
+    }
+
+    public static void main(String[] args) {
+
+    ArrayList<String> brothers = new ArrayList<String>();
+brothers.add("Dick");
+brothers.add("Henry");
+brothers.add("Michael");
+brothers.add("Bob");
+
+System.out.println("brothers:");
+System.out.println(brothers);
+
+    // removing the last item
+    removeLast(brothers);
+
+System.out.println(brothers);
+}
+}
