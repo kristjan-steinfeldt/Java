d2dff4dab6037774aa6ac839a11af12414a955c0
kristjan <kristjan.steinfeldt@voco.ee>
2022-09-08 23:15:55 -0700

    Exercise ex 75 done

---
+++ src/Ex75.java
@@ -0,0 +1,39 @@
+public class Ex75 {
+    public static class DecreasingCounter {
+        private int value;   // object variable that remembers the value of the counter
+
+        public DecreasingCounter(int valueAtStart) {
+            this.value = valueAtStart;
+        }
+
+        public void printValue() {
+            System.out.println("value: " + this.value);
+        }
+
+        public void decrease() {
+            // write here the code that decrements the value of counter by one
+            if (this.value !=0) {
+                this.value--;
+            }
+        }
+        public void reset() {
+            this.value=0;
+        }
+    }
+
+
+        public static void main(String[] args) {
+            DecreasingCounter counter = new DecreasingCounter(1);
+
+            counter.printValue();
+
+            counter.decrease();
+            counter.printValue();
+
+            counter.decrease();
+            counter.printValue();
+        }
+    }
+
+
+
