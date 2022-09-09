2f74355e1c90085f3fb7d55c50154aad2c23b6a5
kristjan <kristjan.steinfeldt@voco.ee>
2022-09-08 23:45:44 -0700

    Exercise ex 76 done

---
+++ src/Ex76.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class Ex76 {
+
+    public static class Menu {
+
+        private ArrayList<String> meals;
+
+        public Menu() {
+            this.meals = new ArrayList<String>();
+        }
+        public void addMeal(String meal) {
+            this.meals.add(meal);
+        }
+        public void printMeals(){
+            this.meals.forEach(System.out::println);        }
+        public void clearMenu(){
+            this.meals.clear();
+        }
+    }
+
+    public static void main(String[] args) {
+        Menu menu=new Menu();
+        menu.addMeal("Hamburger");
+        menu.addMeal("Fish'n'Chips");
+        menu.addMeal("Sauerkraut");
+        menu.printMeals();
+        menu.clearMenu();
+        menu.printMeals();
+    }
+}
